20e8b6ffb26ba6502c2022014d30551ec1a30af1
karmanya <karmanyasingh0506@gmail.com>
2020-11-21 19:06:05 +0530

    chaincreated

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,constrainedLog,chain,bird;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,14 +16,16 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
+    constrainedLog = new Log(230,180,80,PI/2)
+    bird = new Bird(100,100);
+    chain = new Chain(bird.body,constrainedLog.body)
+    ground = new Ground(600,height,1200,20)
+    platform = new Ground(100,300,200,200)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+   
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -34,8 +37,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
-
+    
+    
+    
+    
 }
 
 function draw(){
@@ -58,7 +63,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
     bird.display();
+
     platform.display();
+    constrainedLog.display();
+    chain.display();
+
 }
